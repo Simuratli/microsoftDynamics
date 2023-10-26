0693ac897c02c7e7d4d4546ab47cb494faa018ed
Simuratli Eljan <simuratli@gmail.com>
2023-10-26 23:58:34 +0400

    new

---
+++ main.js
@@ -0,0 +1,29 @@
+const clientIdInput = document.getElementById("clientIdInput");
+const tenantIdInput = document.getElementById("tenantIdInput");
+const crmUrlInput = document.getElementById("crmUrlInput");
+const loginButton = document.getElementById("loginButton");
+const logoutButton = document.getElementById("logoutButton");
+const getAccountsButton = document.getElementById("getAccountsButton");
+const sendAccountsButton = document.getElementById("sendAccountsButton");
+const accountsTable = document.getElementById("accountsTable");
+const accountsTableBody = document.getElementById("accountsTableBody");
+const message = document.getElementById("message");
+
+
+console.log("main js loaded")
+
+function checkCredentialURLs() {
+    if (clientIdInput.value !== '' && tenantIdInput.value !== '' && crmUrlInput.value !== '') {
+        loginButton.removeAttribute('disabled');
+
+        console.log("test is not disabled")
+    } else {
+        loginButton.setAttribute('disabled', 'true');
+        console.log("test is  disabled")
+    }
+}
+
+
+clientIdInput.addEventListener('input', checkCredentialURLs);
+tenantIdInput.addEventListener('input', checkCredentialURLs);
+crmUrlInput.addEventListener('input', checkCredentialURLs);
