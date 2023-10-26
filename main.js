4188ad502e6608fed573c86238f1625f8daff59e
Simuratli Eljan <simuratli@gmail.com>
2023-10-27 00:00:34 +0400

    new

--- main.js
+++ main.js
@@ -1,24 +1,26 @@
 const clientIdInput = document.getElementById("clientIdInput");
 const tenantIdInput = document.getElementById("tenantIdInput");
 const crmUrlInput = document.getElementById("crmUrlInput");
-const loginButton = document.getElementById("loginButton");
-const logoutButton = document.getElementById("logoutButton");
-const getAccountsButton = document.getElementById("getAccountsButton");
-const sendAccountsButton = document.getElementById("sendAccountsButton");
-const accountsTable = document.getElementById("accountsTable");
-const accountsTableBody = document.getElementById("accountsTableBody");
-const message = document.getElementById("message");
+const loginButtonMain = document.getElementById("loginButton");
+
+
+// const logoutButton = document.getElementById("logoutButton");
+// const getAccountsButton = document.getElementById("getAccountsButton");
+// const sendAccountsButton = document.getElementById("sendAccountsButton");
+// const accountsTable = document.getElementById("accountsTable");
+// const accountsTableBody = document.getElementById("accountsTableBody");
+// const message = document.getElementById("message");
 
 
 console.log("main js loaded")
 
 function checkCredentialURLs() {
     if (clientIdInput.value !== '' && tenantIdInput.value !== '' && crmUrlInput.value !== '') {
-        loginButton.removeAttribute('disabled');
+        loginButtonMain.removeAttribute('disabled');
 
         console.log("test is not disabled")
     } else {
-        loginButton.setAttribute('disabled', 'true');
+        loginButtonMain.setAttribute('disabled', 'true');
         console.log("test is  disabled")
     }
 }
