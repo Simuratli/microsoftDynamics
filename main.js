4641ffe2a372bd072a7441edfe8787da27ed6083
Simuratli Eljan <simuratli@gmail.com>
2023-11-30 23:32:06 +0400

    test

--- main.js
+++ main.js
@@ -844,7 +844,7 @@ const updateExistedTableForEditableFields = async (elements, elementsMain, exist
          }
 
          if (element.name === "uds_linkedinprofilecompanyurl") {
-            console.log(element.name,key,'deneme')
+            console.log(element.name,key,'deneme',value)
             if (key === 'uds_linkedinprofilecompanyurl' && value) {
                element.value = value
             } else if (key === 'uds_salesnavigatorcompanyurl' && value) {
