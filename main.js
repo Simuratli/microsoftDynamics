5f66dfa58723137077db58ca41a4ed75e8f1fb4d
Simuratli Eljan <simuratli@gmail.com>
2023-12-01 01:02:20 +0400

    tedt

--- main.js
+++ main.js
@@ -70,12 +70,12 @@ console.log(parameters['test'],'papappraprpa')
 // const decryptedString = decryptedBytes.toString(CryptoJS.enc.Utf8);
 
 
-// console.log("Encrypted String:", decryptedString);
-// Decrypt
-// var bytes  = CryptoJS.AES.decrypt(ciphertext, 'secret key 123');
-// var originalText = bytes.toString(CryptoJS.enc.Utf8);
+console.log("Encrypted String:", decryptedString);
+Decrypt
+var bytes  = CryptoJS.AES.decrypt(parameters['test'], 'secret key 123');
+var originalText = bytes.toString(CryptoJS.enc.Utf8);
 
-// console.log(originalText); // 'my message'
+console.log(originalText,'papappraprpa asd'); // 'my message'
 
 
 
