c70c3fd4b289a7b78ba7f07ab4781e0d91f9fad1
Simuratli Eljan <simuratli@gmail.com>
2023-12-01 00:28:15 +0400

    tedt

--- main.js
+++ main.js
@@ -960,6 +960,7 @@ const goToCrm = async () =>{
          // window.open(`${baseUrl}/main.aspx?pagetype=entityrecord&etn=${data.value[0].fullname}&id=${data.value[0].contactid}`, "_blank");
       }else{
          // window.open(`${baseUrl}/main.aspx?pagetype=entityrecord&etn=${data.value[0].name}&id=${data.value[0].accountid}`, "_blank");
+         console.log(`${baseUrl}/main.aspx?pagetype=entityrecord&etn=${data.value[0].name}&id=${data.value[0].accountid}`)
       }
 
 
