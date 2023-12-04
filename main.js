55a763ff85284004364a26c256c20e6198201fa7
Simuratli Eljan <simuratli@gmail.com>
2023-12-04 13:58:40 +0400

    tet

--- main.js
+++ main.js
@@ -945,28 +945,6 @@ const updateExistedTableForEditableFields = async (elements, elementsMain, exist
    });
 
 
-
-   const obj1Keys = Object.keys(elements).sort();
-   const obj2Keys = Object.keys(existedTableElement).sort();
-
-
-   if (obj1Keys.length !== obj2Keys.length) {
-      console.log(objEqual,'is equal oonbb');
-   } else {
-      const areEqual = obj1Keys.every((key, index) => {
-         const objValue1 = obj1[changeRequestedNames(key)];
-         const objValue2 = obj2[obj2Keys[index]];
-         return objValue1 === objValue2;
-      });
-      if (areEqual) {
-         objEqual = true;
-         console.log(objEqual,'is equal oonbb');
-      } else {
-         console.log(objEqual,'is equal oonbb');
-      }
-   }
-
-
    elements.forEach(element => {
       existedInputs.forEach(existedTableElement => {
          if (changeRequestedNames(element.name) === existedTableElement.name) {
