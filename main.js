08e39a0be4018cff8c1afafd6e59ed626d8fc3fc
Simuratli Eljan <simuratli@gmail.com>
2023-12-05 11:59:39 +0400

    tet

--- main.js
+++ main.js
@@ -864,7 +864,7 @@ const getUserMainRequestObject = async () => {
    const parameters = JSON.parse(params.query);
    const accounts = await filterBackend(`accounts?$filter=contains(uds_linkedincompanyid, '${parameters.customerId}')`)
    let lastName = document.querySelector('.userName').value.split(" ")
-   lastName = lastName.splice(1, 1)
+   lastName = lastName.splice(1, lastName.length)
    console.log(lastName,'lastname querySelector')
    const bodyOfReq = {
       firstname: document.querySelector('.userName').value.split(" ")[0],
