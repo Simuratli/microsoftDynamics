4aa775ddb540c85d42235afa6f7698b6c04ffae1
Simuratli Eljan <simuratli@gmail.com>
2023-12-08 14:12:31 +0400

    test

--- main.js
+++ main.js
@@ -311,7 +311,7 @@ const fillFormElements = async (exist,existedInputs) => {
 
 const existOrNotFunction = async () => {
    
-   loader.style.display = 'grid'
+   showLoader()
    if (parameters['companyName']) {
       const companies = parameters.linkedinCompanyUrl ? await filterBackend(`accounts?$filter=contains(uds_linkedinprofilecompanyurl, '${parameters.linkedinCompanyUrl}')`) : await filterBackend(`accounts?$filter=contains(uds_salesnavigatorcompanyurl, '${parameters.salesCompanyUrl}')`)
       if (companies.value.length !== 0) {
@@ -365,8 +365,6 @@ const existOrNotFunction = async () => {
       await fillFormElements(contacts.value[0], existedInputs);
 
    }
-
-   loader.style.display = 'none'
 }
 
 
