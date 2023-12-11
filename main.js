a5f43f6b9cc55ee142952c3273510817939841c7
Simuratli Eljan <simuratli@gmail.com>
2023-12-11 12:59:40 +0400

    test

--- main.js
+++ main.js
@@ -823,6 +823,7 @@ const updateData = async () => {
             inputsForAddingError.forEach(element=>{
                if(changeRequestedNames(element.name) === errorMessageText.split("'")[1]){
                   element.classList.add("errorInput")
+                  element.nextSibling.style.display = 'block'
                   console.log(element.nextSibling,'errorInput errorInput')
                }
             })
