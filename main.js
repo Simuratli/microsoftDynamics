768f7f683ae1c35221b60436c32648440cd1602d
Simuratli Eljan <simuratli@gmail.com>
2023-12-12 01:25:12 +0400

    lorem

--- main.js
+++ main.js
@@ -1275,6 +1275,7 @@ async function sendDataverse(url, token) {
                   }
                }else{
                   element.classList.remove("errorInput")
+                  console.log(nameOfFieldError,'nameoeferror fiedls')
                }
             });
 
