4dfd78d1aa348409804b3f7c7d3ba21ff09b2025
Simuratli Eljan <simuratli@gmail.com>
2023-12-14 13:24:15 +0400

    test

--- main.js
+++ main.js
@@ -799,7 +799,7 @@ const getUserUpdatedRequestObject = async () => {
    const lastname = document.querySelector('.userNameUpdated').value.split(" ")
    const bodyOfReq = {
       firstname: document.querySelector('.userNameUpdated').value.split(" ")[0],
-      lastname: lastname[1] ? lastname.filter((_, i) => i > 0).join(" ") : " ",
+      lastname: lastname.filter((_, i) => i > 0).join(" "),
       fullname: document.querySelector('.userNameUpdated').value.trim().replace(/\s+/g, ' '),
       jobtitle: document.querySelector('.jobTitleUpdated').value.trim().replace(/\s+/g, ' '),
       address1_name: document.querySelector('.locationUpdated').value.trim().replace(/\s+/g, ' '),
