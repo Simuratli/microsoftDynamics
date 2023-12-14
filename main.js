62e81975f7c29af99f76388b5a844f301a297af7
Simuratli Eljan <simuratli@gmail.com>
2023-12-14 12:25:56 +0400

    test

--- main.js
+++ main.js
@@ -1161,7 +1161,7 @@ inputsForUserDublicateTable.forEach(element => {
    element.addEventListener("input", () => {
       const existedValue = document.querySelector(`[name='${changeRequestedNames(element.name)}']`)?.value
       console.log('i am workings',)
-      element.value = element.value.replace(/\s/g,'')
+      element.value = element.value.replace(/\s/g,' ')
       if (element.value === existedValue) {
         
          element.classList.remove('differentInputMain')
