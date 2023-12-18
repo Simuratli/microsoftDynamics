2fde190f2ca04205f416af252a927ea0e0509e0c
Simuratli Eljan <simuratli@gmail.com>
2023-12-18 15:55:02 +0400

    master

--- main.js
+++ main.js
@@ -1501,7 +1501,7 @@ const addErrorMessage = (currentElement, type,inputName,node,action) => {
    }else{
       node.childNodes[3].innerHTML = 'Not valid'
       node.childNodes[3].style.display = 'block'
-      node.childNodes[3].classList.add("errorInput")
+      node.childNodes[0].classList.add("errorInput")
    }
 
 
@@ -1532,9 +1532,9 @@ const validateInputFields  = (e) => {
    switch (e.target.name) {
       case 'userName':
             if(!validateString(160,e.target.value)){
-               addErrorMessage(e.target, e.currentTarget.parentNode.nodeName.toLowerCase(),e.target.name,e.currentTarget.parentNode,'add')
+               addErrorMessage(e.currentTarget, e.currentTarget.parentNode.nodeName.toLowerCase(),e.target.name,e.currentTarget.parentNode,'add')
             }else{
-               addErrorMessage(e.target, e.currentTarget.parentNode.nodeName.toLowerCase(),e.target.name,e.currentTarget.parentNode,'remove')
+               addErrorMessage(e.currentTarget, e.currentTarget.parentNode.nodeName.toLowerCase(),e.target.name,e.currentTarget.parentNode,'remove')
             }
          break;
    
