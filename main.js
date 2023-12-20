99e690e16895c65d54c558d640460e66f7a66d5e
Simuratli Eljan <simuratli@gmail.com>
2023-12-20 14:56:31 +0400

    error

--- main.js
+++ main.js
@@ -1550,8 +1550,8 @@ const addErrorMessage = (message, currentElement, type, inputName, node, action)
             currentElement.nextElementSibling.remove()
          }
       } else {
-         node.childNodes[1].classList.remove("errorInput")
-         node.childNodes[3].style.display = 'none'
+         node.childNodes[1]?.classList.remove("errorInput")
+         node.childNodes[3]?.style.display = 'none'
          console.log(node.childNodes[3], 'remove sibling if td')
       }
    } else {
